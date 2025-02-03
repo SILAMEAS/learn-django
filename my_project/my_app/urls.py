a03a30f692ee7f1cfa100581c0a90d5bda2d2a9b
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-02-03 20:07:27 +0700

    first commit

--- my_project/my_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path(route="home/",view=views.home,name='home'),
-    path(route="products/",view=views.items,name='products')
-]
